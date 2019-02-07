12618319a565e4b0024a2f683c721f2bcfaabc46
ImprezaRSC <46632364+ImprezaRSC@users.noreply.github.com>
2019-02-07 17:27:35 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+import subprocess
 
 class Kineticinit(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
+        subprocess.call('roscore')      
     @intent_file_handler('kineticinit.intent')
     def handle_kineticinit(self, message):
         self.speak_dialog('kineticinit')
